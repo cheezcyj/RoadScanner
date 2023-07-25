2ae1d37b4ae2bb760ce61d0d87baf2f8c46c2174
최유정 <cheez1248@gmail.com>
2023-07-25 15:05:50 +0900

    maven화

---
+++ src/main/java/com/roadscanner/controller/Hello.java
@@ -0,0 +1,12 @@
+package com.roadscanner.controller;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("hi world kk");
+
+		System.out.println("hi world kk");
+
+	}
+
+}
---
+++ src/main/java/com/roadscanner/controller/HelloJm.java
@@ -0,0 +1,15 @@
+package com.roadscanner.controller;
+
+public class HelloJm {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello, F1");
+		System.out.println("Hello, F1");
+		System.out.println("Hello, F1");
+		System.out.println("Hello, F1");
+		System.out.println("Hello, F1");
+		
+	}
+
+}
